9ac20dc197be626e4f33c1d8788e6407de66e91d
Elmer Thomas <elmer.thomas@sendgrid.com>
2013-06-27 08:19:24 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+The following code is provided for educational purposes only, it is without warranty. 
+
+You may find the companion tutorial in our [documentation](http://sendgrid.com/docs/Integrate/Partners/WindowsAzure.html).
+
+Please direct any questions/comments regarding this code and/or the tutorial to [Elmer Thomas](mailto:elmer.thomas@sendgrid.com). Thanks!
---
+++ playlist/QSTodoListViewController.m
@@ -0,0 +1,248 @@
+﻿// ----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ----------------------------------------------------------------------------
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#import <WindowsAzureMobileServices/WindowsAzureMobileServices.h>
+#import "QSTodoListViewController.h"
+#import "QSTodoService.h"
+
+
+#pragma mark * Private Interface
+
+
+@interface QSTodoListViewController ()
+
+// Private properties
+@property (strong, nonatomic)   QSTodoService   *todoService;
+@property (nonatomic)           BOOL            useRefreshControl;
+
+@end
+
+
+#pragma mark * Implementation
+
+
+@implementation QSTodoListViewController
+
+@synthesize todoService;
+@synthesize itemText;
+@synthesize activityIndicator;
+
+
+#pragma mark * UIView methods
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Create the todoService - this creates the Mobile Service client inside the wrapped service
+    self.todoService = [QSTodoService defaultService];
+    
+    // Set the busy method
+    UIActivityIndicatorView *indicator = self.activityIndicator;
+    self.todoService.busyUpdate = ^(BOOL busy)
+    {
+        if (busy)
+        {
+            [indicator startAnimating];
+        } else
+        {
+            [indicator stopAnimating];
+        }
+    };
+    
+    // add the refresh control to the table (iOS6+ only)
+    [self addRefreshControl];
+    
+    // load the data
+    [self refresh];
+}
+
+- (void) refresh
+{
+    // only activate the refresh control if the feature is available
+    if (self.useRefreshControl == YES) {
+        [self.refreshControl beginRefreshing];
+    }
+    [self.todoService refreshDataOnSuccess:^
+    {
+        if (self.useRefreshControl == YES) {
+            [self.refreshControl endRefreshing];
+        }
+        [self.tableView reloadData];
+    }];
+}
+
+
+#pragma mark * UITableView methods
+
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle
+ forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Find item that was commited for editing (completed)
+    NSDictionary *item = [self.todoService.items objectAtIndex:indexPath.row];
+    
+    // Change the appearance to look greyed out until we remove the item
+    UILabel *label = (UILabel *)[[tableView cellForRowAtIndexPath:indexPath] viewWithTag:1];
+    label.textColor = [UIColor grayColor];
+    
+    // Ask the todoService to set the item's complete value to YES, and remove the row if successful
+    [self.todoService completeItem:item completion:^(NSUInteger index)
+    {  
+        // Remove the row from the UITableView
+        NSIndexPath *indexPath = [NSIndexPath indexPathForRow:index inSection:0];
+        [self.tableView deleteRowsAtIndexPaths:@[ indexPath ]
+                              withRowAnimation:UITableViewRowAnimationTop];
+    }];
+}
+
+-(UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Find the item that is about to be edited
+    NSDictionary *item = [self.todoService.items objectAtIndex:indexPath.row];
+    
+    // If the item is complete, then this is just pending upload. Editing is not allowed
+    if ([[item objectForKey:@"complete"] boolValue])
+    {
+        return UITableViewCellEditingStyleNone;
+    }
+    
+    // Otherwise, allow the delete button to appear
+    return UITableViewCellEditingStyleDelete;
+}
+
+-(NSString *)tableView:(UITableView *)tableView titleForDeleteConfirmationButtonForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Customize the Delete button to say "complete"
+    return @"complete";
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    // Set the label on the cell and make sure the label color is black (in case this cell
+    // has been reused and was previously greyed out
+    UILabel *label = (UILabel *)[cell viewWithTag:1];
+    label.textColor = [UIColor blackColor];
+    NSDictionary *item = [self.todoService.items objectAtIndex:indexPath.row];
+    label.text = [item objectForKey:@"request"];
+    
+    return cell;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Always a single section
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of items in the todoService items array
+    return [self.todoService.items count];
+}
+
+
+#pragma mark * UITextFieldDelegate methods
+
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+
+#pragma mark * UI Actions
+
+
+- (IBAction)onAdd:(id)sender
+{
+    NSString *username = @"<sendgrid_username>";
+    NSString *apikey = @"<sendgrid_pass>";
+    NSString *to = @"<the_playlist_email>";
+    NSString *from = @"<your_from_email>";
+    NSString *fromname = @"<your_from_name>"; // Make sure this text is URL encoded
+    NSString *text = @"%20"; // No body text is needed, but we don't want to leave it blank
+    
+    // Convert your data and set your request's HTTPBody property
+    NSString *subject = [itemText.text stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"http://sendgrid.com/api/mail.send.json?to=%@&from=%@&fromname=%@&subject=%@&text=%@&api_user=%@&api_key=%@",to, from, fromname, subject, text, username, apikey]];
+    
+    // Debugging - displays the final API call in your XCode console
+    NSLog(@"URL %@",url);
+    
+    // Create a request object using the URL.
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    // Prepare for the response back from the server
+    NSHTTPURLResponse *response = nil;
+    NSError *error = nil;
+    
+    // Send a synchronous request to the server (i.e. sit and wait for the response)
+    NSData *responseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+    NSLog(@"Reponse from web:%@", responseData);
+    
+    // Check if an error occurred
+    if (error != nil) {
+        NSLog(@"%@", [error localizedDescription]);
+        // Do something to handle/advise user.
+        
+        UIAlertView *message = [[UIAlertView alloc] initWithTitle:@"Login error"
+                                                          message:@""
+                                                         delegate:nil
+                                                cancelButtonTitle:@"OK"
+                                                otherButtonTitles:nil];
+        [message show];
+    }
+    itemText.text = @"";
+}
+
+
+#pragma mark * iOS Specific Code
+
+// This method will add the UIRefreshControl to the table view if
+// it is available, ie, we are running on iOS 6+
+
+- (void)addRefreshControl
+{
+    Class refreshControlClass = NSClassFromString(@"UIRefreshControl");
+    if (refreshControlClass != nil)
+    {
+        // the refresh control is available, let's add it
+        self.refreshControl = [[UIRefreshControl alloc] init];
+        [self.refreshControl addTarget:self
+                                action:@selector(onRefresh:)
+                      forControlEvents:UIControlEventValueChanged];
+        self.useRefreshControl = YES;
+    }
+}
+
+- (void)onRefresh:(id) sender
+{
+    [self refresh];
+}
+
+
+@end
